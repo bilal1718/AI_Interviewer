2420b47ef51918968288f408bdb12279db25c84c
bilal1718 <muhbil171845@gmail.com>
2024-06-18 13:50:15 +0500

    Apply routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Chat_Bot from './components/Chat_Bot'
 import SignUp from './components/SignUp'
 import Questions from './components/Questions'
+import { useState } from 'react'
 
 
 function App() {
+  const [chatHistory,setChatHistory]=useState([]);
   return(
-    <>
-    <Questions />
-     {/* <Chat_Bot /> */}
-    {/* <SignUp /> */}
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<SignUp />} />
+      <Route path='/questions' element={<Questions
+       chatHistory={chatHistory} setChatHistory={setChatHistory} />} />
+      <Route path='/chatbot' element={<Chat_Bot
+      chatHistory={chatHistory} setChatHistory={setChatHistory} />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
