b7454e189c78d17d64784cd8036b8c97aea65477
Muhammad Bilal <muhbil171845@gmail.com>
2024-06-22 15:48:08 +0500

    New component

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,26 @@ import Chat_Bot from './components/Chat_Bot'
 import SignUp from './components/SignUp'
 import Questions from './components/Questions'
 import { useState } from 'react'
+import Video from './components/video'
 
 
 function App() {
   const [chatHistory,setChatHistory]=useState([]);
   return(
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<SignUp />} />
-      <Route path='/questions'
-       element={<Questions
-       chatHistory={chatHistory} setChatHistory={setChatHistory} />} />
-      <Route path='/chatbot'
-      element={<Chat_Bot
-      chatHistory={chatHistory} setChatHistory={setChatHistory} />} />
-    </Routes>
-    </BrowserRouter>
+    <>
+    <Video />
+    </>
+    // <BrowserRouter>
+    // <Routes>
+    //   <Route path='/' element={<SignUp />} />
+    //   <Route path='/questions'
+    //    element={<Questions
+    //    chatHistory={chatHistory} setChatHistory={setChatHistory} />} />
+    //   <Route path='/chatbot'
+    //   element={<Chat_Bot
+    //   chatHistory={chatHistory} setChatHistory={setChatHistory} />} />
+    // </Routes>
+    // </BrowserRouter>
   )
 }
 
